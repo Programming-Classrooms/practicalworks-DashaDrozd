c46b864d438c8ffc794c1d9d3babef68ce2ab9a0
Dasha Drozd <drozdik6013@gmail.com>
2023-11-29 01:25:47 +0300

    Внесены мелкие изменения (поправки)

--- laboratory-task-1/lab1.cpp
+++ laboratory-task-1/lab1.cpp
@@ -1,23 +1,25 @@
-#define _USE_MATH_DEFINES 
 #include <iostream> 
 #include <cmath> 
+
+
 int main() 
 { 
  double eps, p, s, x; 
  int32_t n; 
+
  std::cout << "input x="; 
  std::cin >> x; 
  std::cout << "input eps="; 
  std::cin >> eps; 
+
  p = x; 
  s = 0; 
  n = 1; 
- if (x < -1 || x >= 1) 
- { 
+
+ if (x < -1 || x >= 1) { 
   std::cout << "error x belongs [-1;1)" << '\n'; 
  } 
- while (fabs(p) >= eps) 
- { 
+ while (fabs(p) >= eps) { 
   s -= p; 
   p = ( p * x ) / n; 
   ++n; 
@@ -25,4 +27,5 @@ int main()
   
  std::cout << "ln(1-x)=" << logf(1 - x) << "\ts=" << s; 
  
+ return 0;
 }
