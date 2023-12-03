cacb3260706cd7b44ed8ca2a22935f6da1f996c9
Dasha Drozd <drozdik6013@gmail.com>
2023-12-03 20:16:35 +0300

    Переназвала функции и внесла поправки в функции в 4 лабе

--- laboratory-task-4/lab4.cpp
+++ laboratory-task-4/lab4.cpp
@@ -6,7 +6,7 @@
 using std::cout; 
 using std::cin; 
  
-void fillKeyboard(int32_t** matrix, int32_t& size) 
+void fillMatrixFromKeyboard(int32_t** matrix, int32_t& size) 
 { 
  cout << "Enter elements of matrix: "; 
  for (size_t i = 0; i < size; ++i)  
@@ -18,7 +18,7 @@ void fillKeyboard(int32_t** matrix, int32_t& size)
  } 
 } 
  
-void fillRand(int32_t** matrix, const int32_t& size) 
+void fillMatrixFromRandom(int32_t** matrix, const int32_t& size) 
 { 
  int32_t min, max; 
  cout << "Enter boarders: "; 
@@ -50,13 +50,13 @@ void printMatrix(int32_t** matrix, const int32_t& size)
  } 
 } 
  
-int32_t searchOfMaxInRightLowConer(int32_t** matrix, const int32_t& size) 
+int32_t searchOfMaxInRightLowCorner(int32_t** matrix, const int32_t& size) 
 { 
  int32_t max = matrix[0][size-1]; 
  
  for (size_t i = 0; i < size; ++i) 
  { 
-  for(size_t j = 0; j < size; ++j) 
+  for(size_t j = size-1; j < size; --j) 
   { 
    if (i > size - 1 - j && matrix[i][j] > max) 
    { 
@@ -67,12 +67,9 @@ int32_t searchOfMaxInRightLowConer(int32_t** matrix, const int32_t& size)
  return max; 
 } 
  
-size_t maxInI(int32_t** matrix, const int32_t& size) 
-{ 
+ void findMaxElemInds(int32_t** matrix, const size_t& size, size_t& rowInd, size_t& colInd ) 
+{
  int32_t max = matrix[0][0]; 
- size_t indi = 0; 
- size_t indj = 0; 
- 
  for (size_t i = 0; i < size; ++i) 
  { 
   for (size_t j = 0; j < size; ++j) 
@@ -80,51 +77,32 @@ size_t maxInI(int32_t** matrix, const int32_t& size)
    if (matrix[size-1-i][size-1-j] > max) 
    { 
     max = matrix[size-1-i][size-1-j]; 
-    indi = size-1-i; 
+    rowInd = size-1-i; 
+    colInd = size-1-j; 
    } 
   } 
  } 
- return indi; 
-} 
- 
-size_t maxInJ(int32_t** matrix, const int32_t& size) 
+}
+
+void movingMaxElementInLeftCorner(int32_t** matrix, const int32_t& size, size_t& rowInd, size_t& colInd) 
 { 
- int32_t max = matrix[0][0]; 
- size_t indi = 0; 
- size_t indj = 0; 
- 
- for (size_t i = 0; i < size; ++i) 
- { 
-  for (size_t j = 0; j < size; ++j) 
-  { 
-   if (matrix[size-1-i][size-1-j] > max) 
-   { 
-    max = matrix[size-1-i][size-1-j]; 
-    indj = size-1-j; 
-   } 
-  } 
- } 
- return indj; 
-} 
- 
-void maxElementInLeftCorner(int32_t** matrix, const int32_t& size) 
-{ 
- if (matrix[maxInI(matrix, size)][maxInJ(matrix, size)] < 0)  
+ if (matrix[rowInd][colInd] < 0)  
  { 
   throw "Maximum element is not positive!"; 
  } 
- for (int32_t i = 0; i < size ; ++i) 
- { 
-    
-  for (size_t j = 0; j <size; ++j) 
-  {    
-   std::swap(matrix[i][0],matrix[maxInI(matrix, size)][maxInJ(matrix, size)]); 
-   std::swap(*matrix[j], *matrix[maxInJ(matrix, size)]); 
-  } 
+   std::swap(*matrix[0],*matrix[rowInd]);  
+    for(size_t i = 0; i < size; ++i)
+    {
+
+      for (size_t j = 0; j < colInd; ++j) 
+         {    
    
- } 
+             std::swap( matrix[i][j], matrix[i][colInd]); 
+         } 
+    }
  printMatrix(matrix, size); 
 } 
+
 void deleteMemory(int32_t** matrix, const int32_t& size) 
 { 
  for (size_t j = 0; j < size; ++j) 
@@ -141,39 +119,47 @@ int main()
 { 
  int32_t** matrix; 
  int32_t n = -1; 
+
  while (n <= 0) 
  { 
   cout << " Enter number of rows and columns: "; 
   cin >> n; 
  } 
-    matrix= new int32_t * [n]; 
+
+ matrix= new int32_t * [n]; 
+
  for (size_t i = 0; i < n; ++i) 
  { 
   matrix[i] = new int32_t [n]; 
  } 
+
  try 
  { 
   srand(time(NULL)); 
   int32_t op; 
   cout << "Enter 0 - keyboard,1 - random :  "; 
   cin >> op; 
+
   switch (op) 
   { 
-  case 0: 
-   fillKeyboard(matrix, n); 
-   break; 
-  case 1: 
-   fillRand(matrix, n); 
-   break; 
-  default: 
-   std::cout << "Input incorrect value.Get out of my program!"; 
-   return 1; 
+   case 0: 
+      fillMatrixFromKeyboard(matrix, n); 
+      break; 
+   case 1: 
+      fillMatrixFromRandom(matrix, n); 
+      break; 
+   default: 
+      std::cout << "Input incorrect value.Get out of my program!"; 
+      return 1; 
   } 
+
+  size_t rowInd = 0;
+  size_t colInd = 0;
   printMatrix(matrix, n); 
-  cout << "\nMaximum element of the lower right triangle of the matrix: " << searchOfMaxInRightCorner(matrix, n) << '\n'; 
-  maxElementInLeftCorner(matrix, n); 
+  cout << "\nMaximum element of the lower right triangle of the matrix: " << searchOfMaxInRightLowCorner(matrix, n) << '\n'; 
+  findMaxElemInds(matrix, n, rowInd, colInd);
+  movingMaxElementInLeftCorner(matrix, n, rowInd, colInd); 
   std::cout << "\n"; 
-  cout << maxInI(matrix, n) << maxInJ(matrix, n) << std::endl; 
   throw "";  
  } 
  
@@ -181,8 +167,7 @@ int main()
  { 
   std::cout << msg; 
   deleteMemory(matrix, n); 
-   
  } 
+
  return 0; 
- 
 }
