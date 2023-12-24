4e8840e7f55711557bba23d91bba5d4f30fc611b
Dasha Drozd <drozdik6013@gmail.com>
2023-12-24 20:36:44 +0300

    Переназвала некоторые функции, переделала функцию ввода, убрала пробелы, добавила пустую строку, добавила условие

--- laboratory-task-5/lab5.cpp
+++ laboratory-task-5/lab5.cpp
@@ -1,3 +1,14 @@
+/*
+    Вычислить значение определенного интеграла с аналитически   
+    функцией с заданной точностью eps
+    А) по формуле левых прямоугольников;
+    Б) по формуле правых прямоугольников;
+    В) по формуле средних прямоугольников;
+    Г) по формуле трапеций;
+    Д) по формуле Симпсона (параболических трапеций).
+    Варианты Б,Г
+*/
+
 #include <iostream> 
 #include <functional> 
 #include <cmath> 
@@ -26,15 +37,13 @@ double trapezoidMethodIntergtal(const std::function<double(double)>& f, double b
    double s1 = 0; 
    double s2 = 0.5 * (f(boarder1) + f(boarder2)) * (boarder2 - boarder1); 
 
-   while (fabs(s2 - s1) > eps)
-   { 
+   while (fabs(s2 - s1) > eps) { 
       s1 = s2; 
       n *= 2; 
       width = (boarder2 - boarder1) / n; 
       s2 = 0; 
       
-         for (int step = 0; step < n; ++step)  
-         { 
+         for (int step = 0; step < n; ++step) { 
             x1 = boarder1 + step * width; 
             x2 = boarder1 + (step + 1) * width; 
             s2 += 0.5 * (x2 - x1) * (f(x1) + f(x2)); 
@@ -53,15 +62,13 @@ double rightRectangleMethodIntergtal(const std::function<double(double)>& f, dou
    double s1 = 0; 
    double s2 = f(boarder2) * (boarder2 - boarder1); 
    
-   while (fabs(s2 - s1) >= eps) 
-   { 
+   while (fabs(s2 - s1) >= eps) { 
       s1 = s2; 
       s2 = 0.0; 
       n *= 2; 
       width = (boarder2 - boarder1) / n; 
       
-         for (int step = 0; step < n; ++step) 
-         { 
+         for (int step = 0; step < n; ++step){ 
             x = boarder1 + step * width; 
             s2 += width * f(x); 
          } 
@@ -71,26 +78,26 @@ double rightRectangleMethodIntergtal(const std::function<double(double)>& f, dou
    return s2; 
 } 
  
-void getBoarders(double& boarder1, double& boarder2) 
+void inputBoarders(double& boarder1, double& boarder2) 
 { 
    std::cout << "Enter the first boarder = "; 
    std::cin >> boarder1; 
    std::cout << "Enter the second boarder = "; 
    std::cin >> boarder2;
 
-   if (boarder1 > boarder2) 
-   {
+   if (boarder1 > boarder2) {
       std::swap(boarder1, boarder2);
    }
    
 
 } 
 
- void getEpsilon(double& eps)
+ void inputEpsilon(double& eps)
  {
-      while (eps <= 0) 
-      { 
-         std::cout << "Enter natural epsilon: "; 
+      std::cout << "Enter natural epsilon: "; 
+      cin >> eps;
+      while (eps < 0 || eps > 1) { 
+         std::cout << "Enter natural epsilon from 1 to 0: "; 
          std::cin >> eps; 
       } 
  }
@@ -99,13 +106,12 @@ int main() {
    double boarder1, boarder2; 
    boarder1 = 0; 
    boarder2 = 0; 
-   double eps = -1; 
-   getEpsilon(eps);
+   double eps = 0; 
+   inputEpsilon(eps);
    double (*funct[3])(double) = {funct1, funct2, funct3}; 
 
-   for (size_t i = 0; i < 3; ++i) 
-   { 
-      getBoarders(boarder1, boarder2); 
+   for (size_t i = 0; i < 3; ++i) { 
+      inputBoarders(boarder1, boarder2); 
       cout << "Intergtal of " << i + 1 << " function by trapezoid method : " << trapezoidMethodIntergtal(funct[i], boarder1, boarder2, eps) << '\n'; 
       cout << "Intergtal of " << i + 1 << " function by right rectangle method : " << rightRectangleMethodIntergtal(funct[i], boarder1, boarder2, eps) << '\n'; 
    } 
